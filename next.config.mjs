b18e8319b296c67769df5b5cae75c261d7f050e6
Geeleed <surasak.kaewpho@gmail.com>
2024-05-04 21:44:10 +0700

    เพิ่ม pwa และ icon

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,28 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: [
+//       "geeleedwinwin.s3.amazonaws.com",
+//       "geeleedwinwin.s3.ap-southeast-2.amazonaws.com",
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+import withPWA from "@ducanh2912/next-pwa";
+
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
   images: {
     domains: [
       "geeleedwinwin.s3.amazonaws.com",
@@ -8,4 +31,15 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options
+})(nextConfig);
